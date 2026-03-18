c50b29d1fd6d00b51b4c4c8ffccecfdc57777c96
Rohan sahu <sahurohankumar47@gmail.com>
2026-03-18 15:29:27 +0530

    typing feature added in socket

--- chat/src/config/socket.ts
+++ chat/src/config/socket.ts
@@ -27,6 +27,39 @@ io.on("connection",(socket:Socket)=>{
 
     io.emit("getOnlineUser",Object.keys(userSocketMap))
 
+    if(userId){
+        socket.join(userId)
+    }
+    socket.on("typing",(data)=>{
+        console.log(`User ${data.userId} is typing in chat ${data.chatId}`);
+        socket.to(data.chatId).emit("userTyping",{
+            chatId:data.chatId,
+            userId:data.userId,
+        })
+        
+    })
+
+    socket.on("stopTyping",(data)=>{
+        console.log(`User ${data.userId} stopped typing in chat ${data.chatId}`);
+        socket.to(data.chatId).emit("userStopTyping",{
+            chatId:data.chatId,
+            userId:data.userId,
+        })
+        
+    })
+
+    socket.on("joinChat",(chatId)=>{
+        socket.join(chatId)
+        console.log(`User ${userId} joined chat ${chatId}`);
+        
+    })
+
+    socket.on("leaveChat",(chatId)=>{
+        socket.leave(chatId)
+        console.log(`User ${userId} left chat ${chatId}`);
+        
+    })
+
     socket.on("disconnect",()=>{
         console.log("user disconnected",socket.id);
         if(userId){
